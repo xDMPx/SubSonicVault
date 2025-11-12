99cdb5a5a77dedf0180c96147bbf9ea661eb397a
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-11-12 20:33:32 +0100

    Make home endpoint list all audio files stored in the base directory

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
+use std::str::FromStr;
+
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
 
 struct AppState {
     base_dir: String,
-}
-
-#[get("/")]
-async fn hello(data: web::Data<AppState>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Base dir set to: {}", data.base_dir))
-}
-
-#[get("/ping")]
-async fn ping() -> impl Responder {
-    HttpResponse::Ok()
 }
 
 #[actix_web::main]
@@ -26,10 +18,62 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(AppState {
                 base_dir: base_dir.clone(),
             }))
-            .service(hello)
-            .service(ping)
+            .service(home)
     })
     .bind(("0.0.0.0", 65421))?
     .run()
     .await
+}
+
+#[get("/")]
+async fn home(data: web::Data<AppState>) -> impl Responder {
+    let files = traverse_dir(&data.base_dir);
+    let files = files.iter().map(|p| format!("{:?}\n", p));
+    let mut files = files.collect::<Vec<String>>();
+    files.sort_unstable();
+
+    HttpResponse::Ok()
+        .content_type("text/plain; charset=utf-8")
+        .body(files.concat())
+}
+
+fn traverse_dir(base_dir: &str) -> Vec<std::path::PathBuf> {
+    let mut dir_list = vec![std::path::PathBuf::from_str(base_dir).unwrap()];
+    let mut audiofiles = Vec::new();
+    while dir_list.len() > 0 {
+        let entries = std::fs::read_dir(dir_list.pop().unwrap()).unwrap();
+        for entry in entries {
+            if let Ok(file) = entry {
+                if let Ok(file_type) = file.file_type() {
+                    if file_type.is_file() && is_audiofile(file.path()) {
+                        audiofiles.push(file.path());
+                    } else if file_type.is_dir() {
+                        dir_list.push(file.path());
+                    }
+                }
+            }
+        }
+    }
+
+    return audiofiles;
+}
+
+fn is_audiofile(path: std::path::PathBuf) -> bool {
+    if let Some(ext) = path.extension() {
+        if ext == "m4b" {
+            return true;
+        } else if ext == "m4a" {
+            return true;
+        } else if ext == "mp3" {
+            return true;
+        } else if ext == "flac" {
+            return true;
+        } else if ext == "wav" {
+            return true;
+        } else if ext == "opus" {
+            return true;
+        }
+    }
+
+    false
 }
