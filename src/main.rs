3770f7954b8db42039fa4ec117772d9e7cdde585
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-11-12 20:00:05 +0100

    Introduce application state and store base directory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+
+struct AppState {
+    base_dir: String,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn hello(data: web::Data<AppState>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Base dir set to: {}", data.base_dir))
 }
 
 #[get("/ping")]
@@ -12,8 +16,20 @@ async fn ping() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(ping))
-        .bind(("0.0.0.0", 65420))?
-        .run()
-        .await
+    let base_dir: String = std::env::args()
+        .skip(1)
+        .next()
+        .expect("Arg with dir location required");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                base_dir: base_dir.clone(),
+            }))
+            .service(hello)
+            .service(ping)
+    })
+    .bind(("0.0.0.0", 65421))?
+    .run()
+    .await
 }
